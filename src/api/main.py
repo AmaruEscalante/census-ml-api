626442591d790a83253a76feae8dcd31dee08d42
Amaru Escalante <amaruescalante@gmail.com>
2023-03-07 23:14:25 -0500

    feat: add fast api server

--- src/api/main.py
+++ src/api/main.py
@@ -1,1 +1,19 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+import tensorflow as tf
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/predict")
+def predict():
+    # Run prediction using the ML model trained to predict the salary of a person
+    # Load ML Model from disk with extension .pb or .h5 using tensorflow or keras
+    model = tf.keras.models.load_model(filepath="../model/saved_model.pb")
+    print("Model loaded correctly")
+
+    return {"Hello": "World"}
