cdb22a2635afb400975f3a5a94af47d215dd8a0b
Amaru Escalante <amaruescalante@gmail.com>
2023-03-18 01:27:29 -0500

    feat: add third test case for api

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -9,7 +9,7 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"OK": 200, "message": "Welcome to the API. Please use /docs to see the documentation."}
 
-def test_predict():
+def test_predict_less_than_50K():
     payload = {
         "age": 40,
         "workclass": "State-gov",
@@ -29,4 +29,28 @@ def test_predict():
     response = client.post("/predict", json=payload)
     assert response.status_code == 200
     assert "prediction" in response.json()
+    assert "<=50K" in response.json()["prediction"]
+
+def test_predict_more_than_50K():
+    payload = {
+        "age": 37,
+        "workclass": "Private",
+        "fnlgt": 280464,
+        "education": "Some-college",
+        "education_num": 10,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "Black",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 80,
+        "native_country": "United-States"
+    }
+
+    response = client.post("/predict", json=payload)
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    assert ">50K" in response.json()["prediction"]
 
