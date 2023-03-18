2a05bf03f019c2c103753152b6cf1da0eb24e426
Amaru Escalante <amaruescalante@gmail.com>
2023-03-18 00:55:54 -0500

    feat(api): improved predict post endpoint to consume the input and predict the income

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
 import tensorflow as tf
+import pandas as pd
+from src.ml.data import process_data
+from src.ml.train_model import cat_features
+from src.ml.model import inference
+import pickle
 
 app = FastAPI()
+to_csv = lambda x: x.replace("_", "-")
+
 
 class Input(BaseModel):
     age: int
@@ -20,6 +27,19 @@ class Input(BaseModel):
     hours_per_week: int
     native_country: str
 
+    class Config:
+        alias_generator = to_csv
+
+    #     schema_extra = {
+    #         "example": {
+    #             "name": "Foo",
+    #             "description": "A very nice Item",
+    #             "price": 35.4,
+    #             "tax": 3.2,
+    #         }
+    #     }
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -27,8 +47,19 @@ def read_root():
 
 @app.post("/predict")
 def predict(input: Input):
-    model = tf.keras.models.load_model(filepath="../model")
-    
-    print(input)
+    model = tf.keras.models.load_model("src/model/model.h5")
+    with open("src/model/encoder.pkl", "rb") as f:
+        encoder = pickle.load(f)
+    with open("src/model/scaler.pkl", "rb") as f:
+        scaler = pickle.load(f)
+    with open("src/model/label_binarizer.pkl", "rb") as f:
+        label_binarizer = pickle.load(f)
 
-    return {"OK": 200}
+    input_df = pd.DataFrame(input.dict(by_alias=True), index=[0])
+
+    X, y, _, _, _ = process_data(
+        input_df, categorical_features=cat_features, training=False, encoder=encoder, scaler=scaler, lb=label_binarizer
+    )
+    preds = inference(model, X)
+    preds = label_binarizer.inverse_transform(preds)
+    return {"OK": 200, "prediction": preds[0]}
