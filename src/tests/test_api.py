edfac40f807baa2fb7cdcc15a3b6a54f969f6910
Amaru Escalante <amaruescalante@gmail.com>
2023-03-18 01:29:24 -0500

    test: fix hyphen formatting

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -37,16 +37,16 @@ def test_predict_more_than_50K():
         "workclass": "Private",
         "fnlgt": 280464,
         "education": "Some-college",
-        "education_num": 10,
-        "marital_status": "Married-civ-spouse",
+        "education-num": 10,
+        "marital-status": "Married-civ-spouse",
         "occupation": "Exec-managerial",
         "relationship": "Husband",
         "race": "Black",
         "sex": "Male",
-        "capital_gain": 0,
-        "capital_loss": 0,
-        "hours_per_week": 80,
-        "native_country": "United-States"
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 80,
+        "native-country": "United-States"
     }
 
     response = client.post("/predict", json=payload)
